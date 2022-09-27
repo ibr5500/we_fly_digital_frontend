7eddff1881840e1256e9ca56e413ee6e63389a3d
KanoCode <kanokanombola@gmail.com>
2022-09-27 22:53:59 +0200

    Add styling to Navigation panel, make responsive

--- src/Components/NavigationPanel.js
+++ src/Components/NavigationPanel.js
@@ -1,49 +1,76 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import logo from '../logo.png';
 
 function NavigationPanel() {
-  return (
-    <div className="nav-panel">
-      <div className="header__logo">
-        <a href="#valid">
-          {' '}
-          <img src={logo} alt="logo" />
-        </a>
-      </div>
-      {/* <h1 className="">𝓕𝓵𝔂 𝓔𝓿𝓮𝓻𝔂𝔀𝓱𝓮𝓻𝓮</h1> */}
-      <ul className="panel-links">
-        <li>Home</li>
-        <li>Add Flight</li>
-        <li>Bookings</li>
-        <li>Reservations </li>
-        <li>Delete Flight</li>
-        <li>Sign Out</li>
-      </ul>
-      <ul className="social-links">
+  const [NavIsActive, toggleActiveNav] = useState(false);
+  const isActive = true;
 
-        <li>
-          <span>
-            <i className="fa-brands fa-linkedin-in" />
-          </span>
-        </li>
-        <li>
-          <span>
-            <i className="fa-brands fa-facebook" />
-          </span>
-        </li>
-        <li>
-          <span>
-            <i className="fa-brands fa-twitter" />
-          </span>
-          {' '}
-        </li>
-        <li>
-          <span>
-            <i className="fa-brands fa-whatsapp" />
-          </span>
-        </li>
-      </ul>
-    </div>
+  return (
+    <>
+      <aside className={NavIsActive ? 'nav-panel hidden' : 'nav-panel'}>
+        <button
+          type="button"
+          className="mobile-toggle"
+          onClick={() => {
+            toggleActiveNav(!NavIsActive);
+          }}
+        >
+          {NavIsActive ? (
+            <i className="fa-solid fa-x" />
+          ) : (
+            <i className="fa-sharp fa-solid fa-bars" />
+          )}
+        </button>
+
+        <div className="header__logo">
+          <Link to="/">
+            {' '}
+            <img src={logo} alt="logo" />
+          </Link>
+        </div>
+
+        <ul className="panel-links">
+          <li className={isActive ? 'active-link' : ''}>
+            <Link to="/">Home</Link>
+          </li>
+          <li className={isActive ? 'active-link' : ''}>
+            <Link onClick={() => toggleActiveNav(!NavIsActive)} to="/reserve">Add Flight</Link>
+          </li>
+          <li className={isActive ? 'active-link' : ''}>
+            <Link onClick={() => toggleActiveNav(!NavIsActive)} to="/cancelled">Cancelled Flight</Link>
+          </li>
+
+          <li className={isActive ? 'active-link' : ''}>
+            {' '}
+            <Link onClick={() => toggleActiveNav(!NavIsActive)} to="/reserved">Reservations</Link>
+          </li>
+          <li>Sign Out</li>
+        </ul>
+        <ul className="social-links">
+          <li>
+            <span>
+              <i className="fa-brands fa-linkedin-in" />
+            </span>
+          </li>
+          <li>
+            <span>
+              <i className="fa-brands fa-facebook" />
+            </span>
+          </li>
+          <li>
+            <span>
+              <i className="fa-brands fa-twitter" />
+            </span>
+          </li>
+          <li>
+            <span>
+              <i className="fa-brands fa-whatsapp" />
+            </span>
+          </li>
+        </ul>
+      </aside>
+    </>
   );
 }
 
