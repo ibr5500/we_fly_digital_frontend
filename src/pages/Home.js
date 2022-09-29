2b9ae0d5cd32534d4331bd6442748e4dc5521c12
unknown <ibr.s.ahmat@gmail.com>
2022-09-29 14:06:01 +0300

    Implement the slider in home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Carousel from 'nuka-carousel/lib/carousel';
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa';
 import { fetchData } from '../redux/details/details';
 
 const Home = () => {
@@ -13,25 +15,46 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="container">
-      { details && details.map((item) => (
-        <div key={item.id} className="card">
-          <div className="img-container">
-            <img src={item.image} alt={item.name} />
-          </div>
-          <div className="contents">
-            <h3>{item.name}</h3>
-            {' '}
-            ................
-            <h4>
-              Price: $
-              {item.price}
-            </h4>
-          </div>
-          <Link to="/details/:id" className="btn-link">View details</Link>
-        </div>
-      ))}
-    </div>
+    <section className="main">
+      <div className="header">
+        <h2>Flights list:</h2>
+        <p>Please book a flight from the following list</p>
+      </div>
+      <div className="container">
+        <Carousel
+          wrapAround={1}
+          slidesToShow={3}
+          adaptiveHeight={1}
+          dragging={0}
+          defaultControlsConfig={{
+            prevButtonText: <FaArrowAltCircleLeft className="icon" />,
+            nextButtonText: <FaArrowAltCircleRight className="icon" />,
+            pagingDotsStyle: {
+              display: 'none',
+            },
+          }}
+        >
+          { details && details.map((item) => (
+            <div key={item.id} className="flights">
+              <Link to="/details/:id" className="link">
+                <div className="img-container">
+                  <img src={item.image} alt={item.name} />
+                </div>
+                <div className="contents">
+                  <h3>{item.name}</h3>
+                  {' '}
+                  ................
+                  <h4>
+                    Price: $
+                    {item.price}
+                  </h4>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </Carousel>
+      </div>
+    </section>
   );
 };
 
