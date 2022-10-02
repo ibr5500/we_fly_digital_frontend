6a4ade110d769afc2b92e7b438a0f77fa7396b5d
KanoCode <kanokanombola@gmail.com>
2022-10-02 22:55:06 +0200

    Add styles for details component

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -5,25 +5,34 @@ import { useParams } from 'react-router-dom';
 const Details = () => {
   const airline = useParams();
   const dataList = useSelector((state) => state.airlines.airlines.data);
-  const airlineDetails = dataList.filter((flight) => +flight.id === +airline.id);
+  const airlineDetails = dataList.filter(
+    (flight) => +flight.id === +airline.id,
+  );
+  console.log(airlineDetails);
 
   return (
-    <div>
+    <div className="container my-5
+     ">
       {airlineDetails.map((item) => (
-        <div key={item.id}>
+        <div className="details-container" key={item.id}>
           <div className="img-container">
             <img src={item.image} alt={item.name} />
           </div>
-          <div className="contents">
-            <h3>{item.name}</h3>
-            {' '}
-            ................
-            <h4>
-              Price: $
-              {item.price}
-            </h4>
+          <div className="contents p-5">
+            <div>
+              <p>Name: </p>
+              <h3>{item.name}</h3>
+            </div>
+
+            <div className="my-2">
+              <p>Price: $</p>
+              {' '}
+              <h4>{item.price}</h4>
+            </div>
+            <button className="btn book-btn my-2" type="button">
+              Book now
+            </button>
           </div>
-          <button type="button">Book now</button>
         </div>
       ))}
     </div>
