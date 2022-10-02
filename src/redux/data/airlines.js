ac68b0c8744e83a0108ded57feea0ce7da13856f
unknown <ibr.s.ahmat@gmail.com>
2022-10-02 13:17:25 +0300

    Fix some errors

---
+++ src/redux/data/airlines.js
@@ -0,0 +1,57 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'; import baseURL from '../api';
+
+const initialState = {
+  airlines: [],
+  error: '',
+};
+
+export const fetchAirlines = createAsyncThunk('airline/fetchAirlines', async () => {
+  const res = await fetch(`${baseURL}/airlines`, {
+    headers: {
+      'Content-type': 'application/json',
+      // Authentication: `Bearer ${loginToken}`, // eslint-disable-line
+    },
+  });
+  const resJson = await res.json();
+  return resJson;
+});
+
+export const fetchNewAirlines = createAsyncThunk('airline/fetchNewAirlines', async (newAirline) => {
+  const res = fetch(`${baseURL}/airlines`, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(newAirline),
+  }).then((res) => res.json());
+  return res;
+});
+
+const airlineSlice = createSlice({
+  name: 'airline',
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(fetchAirlines.fulfilled, (state, action) => {
+      const stateFulfilled = state;
+      stateFulfilled.airlines = action.payload;
+    });
+
+    builder.addCase(fetchAirlines.rejected, (state, action) => {
+      const stateRejected = state;
+      stateRejected.error = action.error.message;
+    });
+
+    builder.addCase(fetchNewAirlines.fulfilled, (state, action) => {
+      const stateFulfilled = state;
+      stateFulfilled.airlines = action.payload;
+    });
+
+    builder.addCase(fetchNewAirlines.rejected, (state, action) => {
+      const stateRejected = state;
+      stateRejected.error = action.payload.error;
+    });
+  },
+});
+
+const airlineReducer = airlineSlice.reducer;
+export default airlineReducer;
---
+++ src/redux/data/details.js
@@ -0,0 +1,30 @@
+const GET_DATA = 'GET_DATA';
+
+export const getDetails = (payload) => ({ type: GET_DATA, payload });
+
+export const fetchData = () => async (dispatch) => {
+  const url = 'http://127.0.0.1:3000/api/v1/airlines';
+  const res = await fetch(url);
+  const resJson = await res.json();
+
+  dispatch(getDetails(resJson));
+};
+
+export const fetchDetails = () => async (dispatch) => {
+  const url = 'http://127.0.0.1:3000/api/v1/airlines';
+  const res = await fetch(url);
+  const resJson = await res.json();
+
+  dispatch(getDetails(resJson));
+};
+
+const detailsReducer = (state = [], action) => {
+  switch (action.type) {
+    case GET_DATA:
+      return action.payload;
+
+    default: return state;
+  }
+};
+
+export default detailsReducer;
