a5f9c6926fcc162b69a39a99a9cfaeb20f1c35b1
unknown <ibr.s.ahmat@gmail.com>
2022-10-04 00:28:03 +0300

    Functionality for delete

--- src/Components/DeletedFlights.js
+++ src/Components/DeletedFlights.js
@@ -1,7 +1,54 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchAirlines, deleteAirline } from '../redux/airlines/airlines';
 
-function DeletedFlights() {
-  return <div>DeletedFlights</div>;
-}
+const DeletedFlights = () => {
+  const dataList = useSelector((state) => state.airlines.airlines.data);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!dataList) dispatch(fetchAirlines());
+  });
+
+  const handleClick = (id) => {
+    dispatch(deleteAirline(id));
+  };
+
+  return (
+    <section className="main">
+      <div className="header">
+        <h2>Flights list:</h2>
+        <p>Please book a flight from the following list</p>
+      </div>
+      <div className="container">
+        <div className="flights">
+          {!(dataList) ? <p>There are no flight yet!!</p>
+            : dataList.map((item) => (
+              <div key={item.id}>
+                <Link to={`/airline/${item.id}`} className="link">
+                  <div className="img-container">
+                    <img src={item.image} alt={item.name} />
+                  </div>
+                  <div className="contents">
+                    <h3>{item.name}</h3>
+                    {' '}
+                    {item.id}
+                    {' '}
+                    ................
+                    <h4>
+                      Price: $
+                      {item.price}
+                    </h4>
+                  </div>
+                </Link>
+                <button type="button" onClick={() => handleClick(item.id)}>Delete</button>
+              </div>
+            ))}
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default DeletedFlights;
