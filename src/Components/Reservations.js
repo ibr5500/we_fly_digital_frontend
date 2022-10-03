c3c22fc5605d58930247269bfca91ee82054da61
NatiG <nati.g5684@gmail.com>
2022-10-03 15:16:36 +0300

    Create reservations UI

--- src/Components/Reservations.js
+++ src/Components/Reservations.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function Reservations() {
+  const reservationsInfo = useSelector((state) => state.reservations.reservations)
+  const airlineInfo = useSelector((state) => state.airlines.airlines)
   return (
-    <div>
-      <h1>BOOK A FLIGHT DESTINATION</h1>
+    <section>
+      <h1>All Reservatoins</h1>
       <p>Travel the world with us</p>
-      {/* { users && users.map((user) => <p key={user.id}>{user.username}</p>)}
-      <Link to="/reservationForm">Book Now</Link> */}
+      { reservationsInfo && reservationsInfo.map((reservation) => (
+        <div>
+          <p>{reservation.city}</p>
+          <p>{reservation.date}</p>
+        </div>
+      ))}
 
-    </div>
+    </section>
   );
 }
 
