b9fedbe71a5ad0dbd59aef4eacdb3e0df90eb036
GASOMINALI Olivier <ogasominali@gmail.com>
2022-10-04 12:48:52 +0200

    Resolve conflict

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchAirlines } from '../redux/airlines/airlines';
 import Airline from './Airline';
 
