11cf80e97a6e1b299f84d27bcba2c024cc863940
KanoCode <kanokanombola@gmail.com>
2022-10-04 16:02:45 +0200

    Update styles for add flight

--- src/Components/AirlineForm.js
+++ src/Components/AirlineForm.js
@@ -26,13 +26,50 @@ function AirlineForm() {
   };
 
   return (
-    <section>
+    <section className="add-airline container">
       <h2>Add New Airline</h2>
       <form onSubmit={handleNewAirline}>
-        <input type="text" value={airline.name} onChange={(e) => setAirline({ ...airline, name: e.target.value })} placeholder="Name" />
-        <input type="text" value={airline.image} onChange={(e) => setAirline({ ...airline, image: e.target.value })} placeholder="Image" />
-        <input type="text" value={airline.price} onChange={(e) => setAirline({ ...airline, price: e.target.value })} placeholder="Price" />
-        <button type="submit">Submit</button>
+        <div className="mb-3">
+          <label htmlFor="airline-name" className="form-label">
+            Airline Name
+            <input
+              type="text"
+              className="form-control"
+              id="airline-name"
+              value={airline.name}
+              onChange={(e) => setAirline({ ...airline, name: e.target.value })}
+              placeholder="Name"
+            />
+          </label>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="image" className="form-label">
+            Airline logo
+            <input
+              type="text"
+              className="form-control"
+              id="image"
+              value={airline.image}
+              onChange={(e) => setAirline({ ...airline, image: e.target.value })}
+              placeholder="Image"
+            />
+          </label>
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="price" className="form-label">
+            Price
+            <input
+              className="form-control"
+              type="text"
+              id="price"
+              value={airline.price}
+              onChange={(e) => setAirline({ ...airline, price: e.target.value })}
+              placeholder="Price"
+            />
+          </label>
+        </div>
+        <button className="btn book-btn" type="submit">Submit</button>
       </form>
     </section>
   );
