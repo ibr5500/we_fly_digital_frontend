76aac5562c3d0c29bea713395af1f9b635898b83
KanoCode <kanokanombola@gmail.com>
2022-10-04 17:25:44 +0200

    update link path in Navigation panel

--- src/Components/NavigationPanel.js
+++ src/Components/NavigationPanel.js
@@ -39,7 +39,7 @@ function NavigationPanel() {
             <Link to="/">Home</Link>
           </li>
           <li className={isActive ? 'active-link' : ''}>
-            <Link onClick={() => toggleActiveNav(!NavIsActive)} to="/reserve">Add Flight</Link>
+            <Link onClick={() => toggleActiveNav(!NavIsActive)} to="/airlineForm">Add Flight</Link>
           </li>
           <li className={isActive ? 'active-link' : ''}>
             <Link onClick={() => toggleActiveNav(!NavIsActive)} to="/delete_flight">Cancelled Flight</Link>
