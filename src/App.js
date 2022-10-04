2bb8a34b25722f09e59576e809c0128cb26c5817
NatiG <nati.g5684@gmail.com>
2022-10-04 16:11:01 +0300

    Render only the login page if loginToken is empty

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
-// import NavigationPanel from './Components/NavigationPanel';
+import NavigationPanel from './Components/NavigationPanel';
 import Home from './Components/Home';
 import DeletedFlights from './Components/DeletedFlights';
 import Reservations from './Components/Reservations';
@@ -11,21 +11,23 @@ import ReservationForm from './Components/ReservationForm';
 import AirlineDetails from './Components/AirlineDetails';
 
 function App() {
-  return (
-    <div>
-      {/* <NavigationPanel /> */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signUp" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/airline/:id" element={<AirlineDetails />} />
-        <Route path="/cancelled" element={<DeletedFlights />} />
-        <Route path="/airlineForm" element={<AirlineForm />} />
-        <Route path="/reservations" element={<Reservations />} />
-        <Route path="/reservationForm/:id" element={<ReservationForm />} />
-      </Routes>
-    </div>
-  );
+  if (sessionStorage.getItem('loginToken')) {
+    return (
+      <div>
+        <NavigationPanel />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/airline/:id" element={<AirlineDetails />} />
+          <Route path="/cancelled" element={<DeletedFlights />} />
+          <Route path="/airlineForm" element={<AirlineForm />} />
+          <Route path="/reservations" element={<Reservations />} />
+          <Route path="/reservationForm/:id" element={<ReservationForm />} />
+        </Routes>
+      </div>
+    );
+  } return <Login />;
 }
 
 export default App;
