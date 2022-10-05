00ba0676a52f17be8704defe860eec6a9e43c430
NatiG <nati.g5684@gmail.com>
2022-10-05 18:20:59 +0300

    Create all tests

---
+++ src/Tests/DeletedFlights.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import renderer from 'react-test-renderer';
+import store from '../redux/configureStore'
+
+import DeletedFlights from '../Components/DeletedFlights';
+
+it('renders deleted page correctly', () => {
+  const tree = renderer.create(<Provider store={store}><DeletedFlights /></Provider>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
---
+++ src/Tests/Home_Snapshot.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import renderer from 'react-test-renderer';
+import store from '../redux/configureStore'
+
+import Home from '../Components/Home';
+
+it('renders home page correctly', () => {
+  const tree = renderer.create(<Provider store={store}><Home /></Provider>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
