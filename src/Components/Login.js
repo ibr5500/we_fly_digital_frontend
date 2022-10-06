d868a5e0b24b4ad8edf785ef345f112de7fb575b
unknown <ibr.s.ahmat@gmail.com>
2022-10-06 20:53:55 +0300

    Fix linter error

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { fetchCurrentUser } from '../redux/users/registerSlice';
 
